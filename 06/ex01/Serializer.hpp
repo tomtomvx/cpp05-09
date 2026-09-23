c7ab189ee45fc7ae980c6f080fb537cc082649b0
TOM VAROUX <tomvaroux@tioemunonotobukkukonpyuta.local>
2026-09-23 17:20:06 +0900

    ファイルだけ作った

---
+++ 06/ex01/Serializer.hpp
@@ -0,0 +1,15 @@
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+class Serializer
+{
+	private:
+		Serializer();
+		Serializer(const Serializer& other);
+		Serializer& operator=(const Serializer& other);
+		~Serializer();
+
+	public:
+}
+
+#endif
---
+++ 06/ex01/main.cpp
@@ -0,0 +1,8 @@
+#include "Serializer.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (1);
+	return (0);
+}
