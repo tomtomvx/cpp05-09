321007d2020a9ef349a6df71a7651a2062e461e9
Cacapon Project <cacapon.project@gmail.com>
2026-09-23 22:38:16 +0900

    generateの実装をした

--- 06/ex02/Base.hpp
+++ 06/ex02/Base.hpp
@@ -6,4 +6,6 @@ class Base {
 		virtual ~Base() = 0; // もしかしたら=0はいらないかもしれない
 };
 
+
+
 #endif
--- 06/ex02/main.cpp
+++ 06/ex02/main.cpp
@@ -1,9 +1,36 @@
 
 #include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 #include <cstddef>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
-Base *generate() { return NULL; }
+Base *generate() { 
+	int random_num = rand() % 3; // 0,1,2
+	if (random_num == 0)
+		return new A;
+	else if (random_num == 1)
+		return new B;
+	else
+		return new C;
+}
 void identify(Base *p) { (void)p; }
 void identify(Base &p) { (void)p; }
 
-int main() { return 0; }
+int main() {
+	std::srand(std::time(NULL));
+	Base* b = generate(); // A,B,Cいずれかが返される
+	(void)b;
+
+	// 禁止されているtypeinfoの確認　デバッグ用
+	// if (typeid(*b) == typeid(A))
+	// 	std::cout << "A" << std::endl;
+	// else if (typeid(*b) == typeid(B))
+	// 	std::cout << "B" << std::endl;
+	// else if (typeid(*b) == typeid(C))
+    //     std::cout << "C" << std::endl;
+	return 0; 
+}
