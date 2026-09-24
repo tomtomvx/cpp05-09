5a17abab26dac25c9bf1f438d14ea02447c2df09
Cacapon Project <cacapon.project@gmail.com>
2026-09-24 22:02:14 +0900

    途中までのiterの処理を定義

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -1,4 +1,13 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <cstddef>
+
+// template <typename T, typename F> こういう書き方はできます
+template<typename T, typename F>
+void iter(T array, const size_t len, F func) {
+	// T array,ではなく別の形で例示されていたので、後でチェックしてね。
+	// TODO: イテレータで行う処理をここに書く
+}
+
 #endif
