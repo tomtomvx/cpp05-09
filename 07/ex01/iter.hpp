fc113f978d5657daf2ebd5ebdda5ec594cdc3e71
TOM VAROUX <tomvaroux@tioemunonotobukkukonpyuta.local>
2026-09-26 15:06:27 +0900

    iterを実装した

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -3,11 +3,11 @@
 
 #include <cstddef>
 
-// template <typename T, typename F> こういう書き方はできます
 template<typename T, typename F>
-void iter(T array, const size_t len, F func) {
-	// T array,ではなく別の形で例示されていたので、後でチェックしてね。
-	// TODO: イテレータで行う処理をここに書く
+void iter(T *array, const std::size_t length, F function) {
+	for (std::size_t i = 0; i < length; ++i) {
+		function(array[i]);
+	}
 }
 
 #endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -1,5 +1,29 @@
 #include "iter.hpp"
 
+#include <iostream>
+
+template<typename T>
+void printElement(const T &element) {
+	std::cout << element << " ";
+}
+
+void addOne(int &element) {
+	++element;
+}
+
 int main() {
+	int numbers[] = {1, 2, 3, 4};
+	const std::size_t count = sizeof(numbers) / sizeof(numbers[0]);
+
+	iter(numbers, count, printElement<int>);
+	std::cout << std::endl;
+
+	iter(numbers, count, addOne);
+	iter(numbers, count, printElement<int>);
+	std::cout << std::endl;
+
+	const char *words[] = {"template", "function"};
+	iter(words, 2, printElement<const char *>);
+	std::cout << std::endl;
 	return 0;
 }
